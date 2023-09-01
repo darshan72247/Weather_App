fa24239936cd8cdbdb8d54723d333a43c06978fe
Darshan Patel <darshanpatel72247@gmail.com>
2023-08-31 23:52:40 -0400

    Removed SearchBar instead added TextField for search and a search button

--- Weather/Controller/MainViewController.swift
+++ Weather/Controller/MainViewController.swift
@@ -9,13 +9,14 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
     
+
     
 }
