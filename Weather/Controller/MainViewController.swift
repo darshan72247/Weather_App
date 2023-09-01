8650d6ae78d9c54b0ab3461c427200f36d9318e4
Darshan Patel <darshanpatel72247@gmail.com>
2023-08-31 23:13:39 -0400

    Removed UISearchView instead added UITextField and SearchButton on MainScreen

---
+++ Weather/Controller/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Weather
+//
+//  Created by Darshan Patel on 2023-08-31.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+}
